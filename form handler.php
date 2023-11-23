44bd1a0ad7e4e624ede43b68d14d66c770929e25
Abdullah8007 <abdullahharishjagrala@gmail.com>
2023-11-23 12:19:17 +0530

    ADD: College-Project

--- form handler.php
+++ form handler.php
@@ -2,14 +2,14 @@
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
-$messege = $_POST['messege'];
+$message = $_POST['message'];
 
 $email_from = 'code@programmer.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                 "User Email: $visitor_email.\n".
                 "Subject: $subject.\n".
-                "User Messege: $messege.\n".;
+                "User Message: $message.\n";
 $to = 'abdullahharishjagrala@gmail.com';
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
