e37a9758d7aa8e2d1d2851a59d656a6dc6498746
Abdullah8007 <abdullahharishjagrala@gmail.com>
2024-02-11 15:16:15 +0530

    123456

--- Pages/contact.php
+++ Pages/contact.php
@@ -1,36 +1,36 @@
 <?php
-var_dump($_POST);
-// $name = $_POST['name'];
-// $email = $_POST['email'];
-// $subject = $_POST['subject'];
-// $message = $_POST['message'];
 
-// $mailheader = "From:".$name."<".$email.">\r\n";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-// $recipient = "abdullahharishjagrala@gmail.com";
+$mailheader = "From:".$name."<".$email.">\r\n";
 
-// mail($recipient, $subject, $message, $mailheader) or die("Error!");
+$recipient = "abdullahharishjagrala@gmail.com";
 
-// echo'
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
 
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Contact form</title>
-//     <link rel="stylesheet" href="style.css">
-// </head>
-// <body>
-//     <div class="container redirect">
-//         <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
-//         <p class="back">Go back to the <a href="index.html">Homepage</a>.</p>
+echo'
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container redirect">
+        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html">Homepage</a>.</p>
         
-//     </div>
-// </body>
-// </html>
+    </div>
+</body>
+</html>
 
-// ';
+';
 
 ?>
